d2b6f823289d7761c7563e126fff333c01cc63ce
Pedro Raposo <peraposo@hotmail.com>
2023-08-31 14:47:19 +0100

    Server creation done

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,9 +1,12 @@
 import { currentProfile } from "@/lib/current-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
-import NavigationAction from "./navigation-action";
 import { Separator } from "@/components/ui/separator";
 import { ScrollArea } from "@/components/ui/scroll-area";
+import NavigationAction from "./navigation-action";
+import NavigationItem from "./navigation-item";
+import { ModeToggle } from "@/components/mode-toggle";
+import { UserButton } from "@clerk/nextjs";
 
 export const NavigationSidebar = async () => {
   const profile = await currentProfile();
@@ -23,28 +26,38 @@ export const NavigationSidebar = async () => {
   });
 
   return (
-    <div className="space-y-2 flex flex-col items-center h-full text-primary w-full dark:bg-[#1e1f22] py-2">
-      <NavigationAction />
-      <Separator
-        className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"
-      />
-      <ScrollArea>
-        <div className="space-y-2">
-          {servers.map((server) => (
-            <div
-              key={server.id}
-              className="flex items-center justify-center group">
-              <div className="flex mx-3 h-[48px] w-[48px] rounded-[48px] bg-white group-hover:rounded-[16px] transition-all overflow-hidden items-center justify-center bg-background dark:bg-neutral-700 group-hover:bg-emerald-500">
-                <img
-                  src={server.imageUrl}
-                  alt={server.name}
-                  className="w-full h-full object-cover"
+    <div className="space-y-4 flex flex-col items-center justify-between h-full text-primary w-full dark:bg-[#1e1f22] py-2">
+      <div className="space-y-4">
+        <NavigationAction />
+        <Separator className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto" />
+        <ScrollArea>
+          <div className="space-y-2">
+            {servers.map((server) => (
+              <div
+                key={server.id}
+                className="flex items-center justify-center group"
+              >
+                <NavigationItem
+                  id={server.id}
+                  imageUrl={server.imageUrl}
+                  name={server.name}
                 />
               </div>
-            </div>
-          ))}
-        </div>
-      </ScrollArea>
+            ))}
+          </div>
+        </ScrollArea>
+      </div>
+      <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+        <ModeToggle />
+        <UserButton
+          afterSignOutUrl="/"
+          appearance={{
+            elements: {
+              avatarBox: "h-[48px] w-[48px]",
+            },
+          }}
+        />
+      </div>
     </div>
   );
 };
